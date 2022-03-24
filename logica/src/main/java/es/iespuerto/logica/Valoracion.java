258f37bab7dca68adc2d8d3df53be5989c8eb1f5
PabloRguezz <71705620+PabloRguezz@users.noreply.github.com>
2022-03-24 19:13:17 +0000

    Añadiendo la documentación de la clase valoracion #2

--- logica/src/main/java/es/iespuerto/logica/Valoracion.java
+++ logica/src/main/java/es/iespuerto/logica/Valoracion.java
@@ -2,6 +2,9 @@ package es.iespuerto.logica;
 public class Valoracion {
     private int valor;
     private String comentario;
+    /**
+     * Metodo para realizar una valoracion del restaurante
+     */
     public void realizarValoracion(){
         
     }        
