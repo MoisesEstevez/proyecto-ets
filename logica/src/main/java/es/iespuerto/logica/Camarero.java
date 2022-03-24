86437d6e914820c584e2a622f3ed4117d669e0ce
PabloRguezz <71705620+PabloRguezz@users.noreply.github.com>
2022-03-24 19:04:56 +0000

    Añadiendo la documentación de la clase camarero #2

--- logica/src/main/java/es/iespuerto/logica/Camarero.java
+++ logica/src/main/java/es/iespuerto/logica/Camarero.java
@@ -1,6 +1,9 @@
 package es.iespuerto.logica;
 
 public class Camarero extends Cliente {
+    /**
+     * Metodo para registrar la mesa de la que el camarero realiza el pedido
+     */
     public void registrarMesaPedido(){
         
     }
